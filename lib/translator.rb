6a8930c5f54966f6de9a956420c06f79ca67ecd2
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-05 19:58:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,7 @@
 require "yaml"
 
-def load_library
+def load_library(file)
+  liabry = {"get meaning" : {}, "get_emoticon" :{}}
+  yaml.load_file(file).each do |char_meaning, arr|
+    eng, japn = arr
+    liabry["get"]
