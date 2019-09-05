7940ff707d3b3b425b3cc60d5a25cfba1d2c74de
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-05 20:40:12 +0000

    Automatically backed up by Learn

---
+++ lib/translator.rb
@@ -0,0 +1,14 @@
+require "yaml"
+
+def load_library(file)
+  hash = {"get_meaning" => {}, "get_emoticon" => {}}
+  YAML.load_file(file).each do |char_meaning, list|
+    eng, japn = list
+    hash["get_emoticon"][eng] = japn
+    hash["get_meaning"][japn] = char_meaning
+  end
+  hash
+end
+
+def get_japanese_emoticon(yml,emotico)
+end
