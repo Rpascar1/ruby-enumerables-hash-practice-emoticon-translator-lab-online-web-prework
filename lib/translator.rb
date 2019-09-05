2706373e4424d20bab07a590b429a721014b024b
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-05 18:04:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,6 @@ require "yaml"
 def load_library(file)
   newhash ={:get_meaning => {}, :get_emoticon => {}}
   emote = YAML.load_file(file)
-  file.each do |get_emoticon, get_meaning|
-    newhash[get]
   end
   newhash
 end
