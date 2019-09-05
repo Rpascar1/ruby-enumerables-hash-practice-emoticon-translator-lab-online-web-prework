e1bec0778c46b32c22aabf3d6ec91ff8bbfbddf1
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-05 18:20:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,10 @@
 require 'yaml'
 
 def load_library(file)
- 
+  library = {"get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file(file).each do |meaning,emoticon|
+  end
+  library
 end    
 
 
